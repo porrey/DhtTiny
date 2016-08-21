c8f91542ec0e29e3cc68b1187264cb46ac74c823
Daniel Porrey <Daniel.Porrey@hotmail.com>
2016-08-20 22:21:51 -0500

    Added copyright and license information.

--- Arduino/DHT_Tiny_Breakout/Debug.h
+++ Arduino/DHT_Tiny_Breakout/Debug.h
@@ -1,3 +1,21 @@
+// Copyright © 2016 Daniel Porrey. All Rights Reserved.
+//
+// This file is part of the DHT Tiny project.
+// 
+// DHT Tiny is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// DHT Tiny is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with DHT Tiny. If not, 
+// see http://www.gnu.org/licenses/.
+//
 #ifndef DEBUG_H
 #define DEBUG_H
 
